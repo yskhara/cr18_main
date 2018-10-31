e30183488cd5e96d04820cef6793770616ab2ee6
yusaku <yskhara97@gmail.com>
2018-11-01 04:49:05 +0900

    stands by

--- include/ControllerCommands.hpp
+++ include/ControllerCommands.hpp
@@ -98,30 +98,23 @@ const std::vector<ControllerCommands> CrMain::route1_pp4_op_commands( {
         ControllerCommands::shutdown, });
 
 const std::vector<ControllerCommands> CrMain::route2_op_commands( {
-//ControllerCommands::standby,
-
-        ControllerCommands::checkpoint_pp1,
+    //standby at sz
+            ControllerCommands::standby,
 
         // pickup at pp1
         ControllerCommands::set_lift_p, ControllerCommands::move_to_pp1, ControllerCommands::pp_pickup,
         // deliver at dp2 @ 1st floor
         ControllerCommands::set_lift_1, ControllerCommands::move_to_dp2, ControllerCommands::dp_deliver,
 
-        ControllerCommands::checkpoint_pp2,
-
         // pickup at pp2
         ControllerCommands::set_lift_p, ControllerCommands::move_to_pp2, ControllerCommands::pp_pickup,
         // deliver at dp2 @ 2nd floor
         ControllerCommands::set_lift_2, ControllerCommands::move_to_dp2, ControllerCommands::dp_deliver,
 
-        ControllerCommands::checkpoint_pp3,
-
         // pickup at pp3
         ControllerCommands::set_lift_p, ControllerCommands::move_to_pp3, ControllerCommands::pp_pickup,
         // deliver at dp3 @ 2nd floor
         ControllerCommands::set_lift_2, ControllerCommands::move_to_dp3, ControllerCommands::dp_deliver,
-
-        ControllerCommands::checkpoint_pp4,
 
         // pickup at pp4
         ControllerCommands::set_lift_p, ControllerCommands::move_to_pp4, ControllerCommands::pp_pickup,
